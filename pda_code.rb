1d8afb92a281cc20ea8275f8aab590e56da56715
Ricky <rickycorrigan@icloud.com>
2018-09-19 12:46:16 +0100

    pda initial commit

---
+++ pda_code.rb
@@ -0,0 +1,33 @@
+
+# fruits = ['apple', 'banana', 'orange', 'grapes']
+#
+# fruits.push('pear')
+# p fruits
+
+
+# def count_fruits
+#   p fruits.push("pear")
+# end
+
+
+# avengers = {
+#   ironman: {
+#     name: "Tony_Stark",
+#     friends:["pepper", "happy"],
+#     moves:{punch: 10,
+#     kick: 100},
+#   },
+#     hulk: {
+#     name: "Bruce_Banner",
+#     moves:{smash: 1000,
+#     roll: 500}
+#     }
+#   }
+#
+# p avengers[:ironman][:friends][1]
+
+# def Film.all()
+#   sql = "SELECT * FROM films"
+#   films_hashes = SqlRunner.run(sql)
+#   return films_hashes.map{|film_hash| Film.new(film_hash)}
+# end
